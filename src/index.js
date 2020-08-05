178d0f21bf46868e369cdc6e27db3b912f4574e6
Vinicius Lago Gehrke <vinictus@gmail.com>
2020-08-05 23:58:17 +0000

    finished database correction, added API service, adding videos

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CadastroVideo from './pages/cadastro/Video';
 import CadastroCategoria from './pages/cadastro/Categoria';
-import Pagina404 from './pages/404';
+
+// Desafio master blaster na descrição
+const Pagina404 = () => (<div>Página 404</div>)
 
 ReactDOM.render(
   <BrowserRouter>
